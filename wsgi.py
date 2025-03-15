1330e533eff6a653ab366b26cd1bb58c24ab7259
Mark <41327826+Mark-RI@users.noreply.github.com>
2025-03-15 01:55:29 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,3 +4,4 @@ from app import app
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Get the port from environment or use 5000 for local
     app.run(host="0.0.0.0", port=port)
+#gunicorn wsgi:app --bind 0.0.0.0:$PORT
